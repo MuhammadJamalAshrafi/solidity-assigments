8acafabb16603e8815d7410060293349d259b419
Muhammad Jamal Ashrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2022-08-21 22:39:43 +0500

    Objects

---
+++ Task1-2.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Objects {
+
+    //Task 1
+    string public welcome = "Hello Solidity";
+
+    //Task 2
+    uint stateVar = 10;
+
+    function returnStateVariable() public view returns(uint) {
+        return stateVar;
+    }
+
+    function returnLocalVariable() public pure returns(uint) {
+        uint localVar = 20;
+        return localVar;
+    }
+}
